cf1a13e6db347fb6a2a0324a2ebc389ca672e11a
Adam Sumner <adam@bristlesandkeys.com>
2021-05-13 16:13:01 +0100

    Create page template with inline style sheet

--- inc/Styles/Component.php
+++ inc/Styles/Component.php
@@ -312,6 +312,12 @@ class Component implements Component_Interface, Templating_Component_Interface {
 					return 'front-page.php' === basename( $template );
 				},
 			),
+			'wp-rig-page'    => array(
+				'file'             => 'page.min.css',
+				'preload_callback' => function() {
+					return is_page();
+				},
+			),
 		);
 
 		/**
