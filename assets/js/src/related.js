edbce8cf217f64ac64a15b064ce75795f39faf13
Adam Sumner <adam@bristlesandkeys.com>
2021-05-14 16:20:34 +0100

    Add js to the related posts component
    * Create releated.js file with initial functions
    * Add function get_category_ids() to related posts component
    * Enqueue script in related posts component

---
+++ assets/js/src/related.js
@@ -0,0 +1,26 @@
+/**
+ * Fetch up to 3 related posts based on current post categories
+ * via the WP REST API. Post are only fetched if and when the
+ * user scrolls the viewport to the container.
+ *
+ * To be placed in assets/js/src/related.js
+ */
+
+// Variables from WordPress
+const postID = postdata.post_ID;
+const catIDs = postdata.cat_ids;
+const restURL = postdata.rest_url;
+
+// Create query URL for the REST API. Note &_embed=true at the end which brings in featured images.
+const queryURL = `${restURL}posts?per_page=3&categories=${catIDs}&exclude=${postID}&_embed=true`;
+
+// Fetch the query results from WP REST API.
+function sendRESTquery() {
+	fetch(queryURL)
+		.then((response) => response.json())
+		.then((data) => console.info(data));
+}
+
+console.info("Hello World!");
+
+sendRESTquery();
